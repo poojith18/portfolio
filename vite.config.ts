13570039cc46e9a81cc969dd9f40a8144d3625e5
Poojith Maligireddy <poojith@poojiths-mbp.lan>
2026-03-13 18:37:25 -0600

    added base in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/portfolio/",
+  base: mode === "production" ? "/portfolio/" : "/", // <-- dev uses "/", prod uses "/portfolio/"
   server: {
     host: "::",
     port: 8080,
